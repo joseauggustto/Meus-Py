56b9bd5ff45a2ab16364652382f5044964985523
joseauggustto <jose.morais22aluno@gmail.com>
2026-02-22 10:48:06 -0300

    Teste

--- Apresentação de nomes.py
+++ Apresentação de nomes.py
@@ -2,4 +2,4 @@ nome = input("Digite seu nome: ")
 idade = input("Digite sua idade: ")
 altura = input("Digite sua altura: ")
 
-print(f"Olá", nome, ",você tem", idade, "anos", "e sua altura é", altura, "metros")
+print(f"Hello", nome, ",você tem", idade, "anos", "e sua altura é", altura, "metros")
