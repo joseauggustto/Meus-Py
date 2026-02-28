c6520e0340a51879f7888c723e756a298d408c70
joseauggustto <jose.morais22aluno@gmail.com>
2026-02-28 15:44:22 -0300

    Em processo

---
+++ Comandos Git.py
@@ -0,0 +1,5 @@
+#git status
+#git add .
+#git commit -m "Mensagem"
+#git push 
+#git restore . (Botão do pânico)
---
+++ Python FCC.py
@@ -0,0 +1,10 @@
+name = 'José'
+age = 22 
+score = 80.9
+altura = 1.89
+
+print(name ,type(name))
+print(age, type(age))
+print(score, isinstance(score, float))
+print(altura, isinstance(altura, float))
+
