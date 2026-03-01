2a3ed3648925f7521e47feebc2eaf1f540e13632
joseauggustto <jose.morais22aluno@gmail.com>
2026-03-01 12:03:31 -0300

    Continuando

--- Python FCC.py
+++ Python FCC.py
@@ -3,8 +3,21 @@ age = 22
 score = 80.9
 altura = 1.89
 
+# Comando isinstance e type. 
+ 
 print(name ,type(name))
 print(age, type(age))
 print(score, isinstance(score, float))
 print(altura, isinstance(altura, float))
 
+#Aspas duplas para a String, informando que é uma citação.
+
+print('She said, "Hello"')
+my_str = "Hello"
+
+#Comando in, usado para saber se existe aquilo na string
+
+print("Hello" in my_str)
+print('World' in my_str)
+
+print(len(my_str))
