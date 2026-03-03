95b14ecbbca2e65a8a5dead456165df197b4e68f
joseauggustto <jose.morais22aluno@gmail.com>
2026-03-03 16:18:15 -0300

    Tarde de terça

--- Operadores Aritméticos.py
+++ Operadores Aritméticos.py
@@ -7,8 +7,56 @@
 # % é igual ao resto da divisão
 # Dois simbolos de igual (==) é usado para testar os operadores
 # Ordem de precedência = Primeiro () ; Segundo (**) ; Terceiro é (*, /, //, %); Em Quarto é (+ e -).
-def soma(a, b)
-    soma = a + b 
-a = 5 
-b = 7 
-print(soma)
+
+#--------------------------------
+
+#Números inteiros e flutuantes, para os mais chegados: int e float.
+
+my_int_number_1 = 56
+my_int_number_2 = -6 
+
+print(type(my_int_number_1))
+print(type(my_int_number_2))
+
+# Soma
+
+sum_ints = my_int_number_1 + my_int_number_2
+print(sum_ints, type(sum_ints))
+
+#Diferença
+
+diff_ints = my_int_number_1 - my_int_number_2 
+print(diff_ints)
+
+# Números Float ----------------------------
+
+my_float_number_1 = 30.5
+my_float_number_2 = 6.5
+
+# Soma dos Flutuadores
+
+sum_float = my_float_number_1 + my_float_number_2
+print(sum_float)
+
+# Diferença dos Flutuadores 
+
+diff_float = my_float_number_1 - my_float_number_2
+print(diff_float)
+
+# ----------------------------------
+
+#Posso converter um número inteiro para flutuante e vice versa.
+
+my_int_number_3 = 70
+my_float_number_3 = 70.0
+
+conversion_int_to_float = float(my_int_number_3)
+conversion_float_to_int = int(my_float_number_3)
+
+#Posso arredondar números com o comando round 
+
+round_number_1 = 2.345
+round_number_2 = 3.234
+
+print(round(round_number_1))
+print(round(round_number_2, 2))
