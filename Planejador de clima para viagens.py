28cd6e7915d2e4bfaad2bb1948a3e1c0960cc0ed
joseauggustto <jose.morais22aluno@gmail.com>
2026-03-04 20:30:02 -0300

    Consegui

--- Planejador de clima para viagens.py
+++ Planejador de clima para viagens.py
@@ -9,30 +9,32 @@ has_ride_share_app = True
 # -------------------------
 
 # Curta distância - até 1 milha. 
-
-if distance_mi < 1:
+if distance_mi == 0:
+    print(False)
+elif distance_mi <= 1:
     if is_raining == True:
-        print('False')
+        print(False)
     elif is_raining == False:
-        print('False')
-
+        print(True)
 
 # Média distância 
 
-#if distance_mi > 1 and distance_mi <= 6 and is_raining == True and has_bike == False: #18
+if distance_mi > 1 and distance_mi <= 6:
+    if is_raining == True and has_bike == False:
+       print(False)
+    elif is_raining == True and has_bike == False: 
+        print(False)
+    elif has_bike == True and is_raining == False:
+        print(True)
     
-#if distance_mi > 1 and distance_mi <= 6 and is_raining == False and has_bike == False: #19
     
-
-#if distance_mi >1 and distance_mi <=6 and has_bike == True and is_raining == False: #20
-    
-
 # Longa distância
 
-#if distance_mi > 6 and has_ride_share_app == True: #21
-    
+if distance_mi > 6:
+    if has_ride_share_app == True:
+        print(True)
+    elif has_car == True:
+        print(True)
+    elif has_car == False and has_ride_share_app == False:
+        print(False)
 
-#if distance_mi > 6 and has_car == True: #22
-    
-
-#if distance_mi > 6 and has_car == False and has_ride_share_app == False: #23
