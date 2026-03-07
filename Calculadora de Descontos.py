93414d5d9b1b22b65cb3caa3d8d3517fd7807a40
joseauggustto <jose.morais22aluno@gmail.com>
2026-03-07 10:28:20 -0300

    Tá saindo...

---
+++ Calculadora de Descontos.py
@@ -0,0 +1,7 @@
+#here go again
+
+def apply_discount(price, discount):
+    print(apply_discount)
+
+
+    
--- Funções.py
+++ Funções.py
@@ -15,7 +15,7 @@ print(int('42'))
 # Para escrever funções customizadas é só usar o comando def seguido do nome que você deseja dar a função, um par de parênteses e dois pontos.
 
 def sum():
-    print(3 + 2)    
+    print(3 + 2)  #Isso aqui se chama identização, esse recuo para a direita.1 
 sum()
 
 # Função de soma mais completa:
@@ -24,26 +24,62 @@ def calculate(a, b, c):  # Essas letras são os parâmetros para a função.
     print(a * b + c)  # Aqui eu vou decidir o que cada um vai fazer, eu acho...
 calculate(4, 4, 4)
 
-# Regra de LEGB do python: Escopo Local, Escopo Envolvente, Escopo Local e Escopo Embutido
+print('-' * 20)
 
-# Escolpo local: A variável declarada só pode ser acessada dentro da função:
+#Pequeno teste de combustpivel do gemini:
 
-def aqui():
-    ali = 20 
-    print(ali) #Só posso aqui
-aqui()
-print(aqui) #Isso vai dar erro
+def verificar_combustível(quantidade):
+    print(f'Verificando: {quantidade} litros...')
+    return quantidade > 10
 
-# Escopo envolvente: significa que uma função que está aninhada dentro de outra função pode acessar as variáveis da função na qual está aninhada.
+resultado1 = verificar_combustível(5)
+print(resultado1)
 
-def outer_func():
-    msg = 'Hello there!'
+resultado2 = verificar_combustível(13)
+print(resultado2)
 
-    def inner_func():
-        print(msg)
+print('-' * 20)
 
-    inner_func()
+# Porteiro da balada:
 
-outer_func()
+def pode_entrar(idade):
+    print(idade)
+    return idade > 18 
 
-print('Não terminou o estudo')
+teste1 = pode_entrar(19)
+print(teste1)
+
+teste2 = pode_entrar(15)
+print(teste2)
+
+print('-' * 30)
+
+# Desafio de desconto 
+
+def calcular_preço_final(valor_produto):
+    if valor_produto > 100:
+        return valor_produto - 20
+    else:
+        return valor_produto
+
+teste1 = calcular_preço_final(150)
+print(teste1)
+
+teste2 = calcular_preço_final(80)
+print(teste2)
+
+print('-' * 30)
+#Desafio final: Analista de Desempenho
+
+def analisar_vendas(nome_vendedor, valor_vendas):
+    if valor_vendas >= 500:
+        return (f'Parabéns {nome_vendedor}, você bateu a meta!')
+
+    else:
+        return (f'Vamo que vamo {nome_vendedor}, falta pouco!')
+
+teste3 = analisar_vendas('João', 600)
+print(teste3)
+
+teste4 = analisar_vendas('Maria', 300)
+print(teste4)
