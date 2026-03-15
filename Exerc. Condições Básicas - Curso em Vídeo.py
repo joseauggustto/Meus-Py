c855e892d32af7b41ebf835cbf2935b724fab423
joseauggustto <jose.morais22aluno@gmail.com>
2026-03-15 13:18:37 -0300

    Tá indo...

--- Exerc. Condições Básicas - Curso em Vídeo.py
+++ Exerc. Condições Básicas - Curso em Vídeo.py
@@ -1,3 +1,98 @@
 # let's go 
 
+# if condicao:
+#     # bloco de código se a condição for verdadeira
+# elif outra_condicao:
+#     # bloco opcional se a primeira for falsa e esta for verdadeira
+# else:
+#     # bloco se nenhuma das anteriores for verdadeira
 
+print("-" * 30)
+
+# run = "S"
+
+# while run == "S":
+#     velocity = float(input("Qual foi a sua velocidade? ")) 
+
+#     if velocity > 80:
+
+#         multa = (velocity - 80) * 5
+
+#         print(f"Você foi multado! Sua multa será de: R${multa:.2f}")
+
+#     else: 
+#         print("Velocidade ideal.")
+
+#     run = input("Nova velocidade? ").upper()                            (Desafio 17 ok)
+
+# print("Fim")
+
+print("-" * 30)
+
+# age = int(input("What are your age? "))
+
+# if age >= 18:
+#     print("Você pode votar!")
+
+# else:
+#     print('Você não pode votar.')         (Desafio 18 ok)
+
+print("-" * 30)
+
+# name = input("What's your name? ")
+# score1 = float(input("What's your first score? "))
+# score2 = float(input("What's your second score? "))
+
+# media = (score1 + score2) / 2 
+
+# if media >= 7.0:
+#     print("Aluno:", name, "- Sua média foi:", media, ", você não se fudeu.")
+
+# else:
+#     print("Aluno:", name, "- Sua média foi:", media, ", você se fudeu.")         (Desafio 19 ok)
+
+print("-" * 30)
+
+# dnv = "S"
+
+# while dnv == "S":
+
+#     num = float(input('Número: '))
+    
+#     if num % 1 != 0:
+#         print("Por favor, digite números inteiros.")
+
+#     else:
+#         p_or_i = num % 2
+    
+#         if p_or_i == 0:
+#             print("Esse número é par.")
+
+#         else:
+#             print("Esse número é impar.")
+
+#     dnv = input("Novamente? ").upper()
+
+# print("Ok")                                               (Desafio 20 ok)
+
+print("-" * 30)
+
+# ano = int(input("Digite o ano: "))
+                    
+# if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+#     print("Esse ano é bissexto")
+
+# else:
+#      print("Esse ano não é Bissexto")     (Desafio 21 ok)
+
+print("-" * 30)
+
+def idade(idade):
+    
+    if idade > 18:
+        return idade - 18
+    
+    else:
+        return 18 - idade
+    
+test = idade()
