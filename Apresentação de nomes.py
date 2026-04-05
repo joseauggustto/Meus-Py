7ccf4fef0d804298d7461fe8ba2707c00a4b5982
joseauggustto <jose.morais22aluno@gmail.com>
2026-04-05 08:19:47 -0300

    Domingo...

--- Apresentação de nomes.py
+++
@@ -1,5 +0,0 @@
-nome = input("Digite seu nome: ")
-idade = input("Digite sua idade: ")
-altura = input("Digite sua altura: ")
-
-print(f"Hello", nome, ",você tem", idade, "anos", "e sua altura é", altura, "meros")
